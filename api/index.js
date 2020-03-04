9d7f3a4251a8f5711eeca05bc3612a25ca9c1d2b
imfsdev <tasnimali0922@gmail.com>
2020-03-04 08:56:00 -0600

    construct

---
+++ api/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import mongoose from "mongoose";
+import schema from "./schema";
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(process.env.db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(
+    () => {
+      console.log("Database is connected");
+    },
+    err => {
+      console.log("Can not connect to the database" + err);
+    }
+  );
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "notetaking api v1"
+  });
+});
+
+app.use("/graphql", graphqlHTTP({ schema: schema, graphiql: true }));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on PORT ${process.env.PORT}`);
+});
